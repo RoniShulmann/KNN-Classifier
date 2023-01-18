311386999398bcaac79c1ca71220fee38abe86d3
Ori <orinurieli1@gmail.com>
2023-01-18 22:59:20 +0200

    finish Ex 2

--- classification-task2/main.py
+++ classification-task2/main.py
@@ -1,17 +1,56 @@
+from collections import Counter
+# from sklearn import preprocessing
 import time
 import json
 import pandas as pd
+import numpy as np
 
 from sklearn.metrics import accuracy_score
+
 from typing import List
 
 
 def classify_with_NNR(data_trn, data_vld, data_tst) -> List:
-    # todo: implement this function
-    print(f'starting classification with {data_trn}, {data_vld}, and {data_tst}')
+    print(
+        f'starting classification with {data_trn}, {data_vld}, and {data_tst}')
+
+    # todo: return a list of your predictions for test instances
+    predictions = KNN(k=3, data_trn=data_trn,
+                      data_vld=data_vld, data_tst=data_tst)
+    return predictions
 
 
-    predictions = list()  # todo: return a list of your predictions for test instances
+def KNN(k, data_trn, data_vld, data_tst):
+    data_trn_df = pd.read_csv(data_trn)
+    data_vld_df = pd.read_csv(data_vld)
+    data_tst_df = pd.read_csv(data_tst)
+
+    # Get the feature and label arrays
+    X_trn, y_trn = data_trn_df.values[:, :-1], data_trn_df['class'].values
+    X_vld, y_vld = data_vld_df.values[:, :-1], data_vld_df['class'].values
+    X_tst, y_tst = data_tst_df.values[:, :-1], data_tst_df['class'].values
+
+    # Initialize an empty list to store the predictions
+    predictions = []
+
+    # Iterate over the test instances
+    for i in range(len(X_tst)):
+        # Find the euclidean distance between the test point and all training points
+        distances = np.sqrt(
+            np.sum((X_trn.astype('float64') - X_tst[i][np.newaxis, :].astype('float64'))**2, axis=1))
+
+        # Get the indices of the k nearest neighbors
+        k_nearest_indices = np.argsort(distances)[: k]
+
+        # Get the labels of the k nearest neighbors
+        k_nearest_labels = y_trn[k_nearest_indices]
+
+        # Get the most common label among the k nearest neighbors
+        most_common_label = Counter(k_nearest_labels).most_common(1)[0][0]
+
+        # Append the prediction to the list
+        predictions.append(most_common_label)
+
     return predictions
 
 
@@ -31,7 +70,9 @@ if __name__ == '__main__':
     if not predicted:  # empty prediction, should not happen in your implementation
         predicted = list(range(len(labels)))
 
-    assert(len(labels) == len(predicted))  # make sure you predict label for all test instances
-    print(f'test set classification accuracy: {accuracy_score(labels, predicted)}')
+    # make sure you predict label for all test instances
+    assert (len(labels) == len(predicted))
+    print(
+        f'test set classification accuracy: {accuracy_score(labels, predicted)}')
 
     print(f'total time: {round(time.time()-start, 0)} sec')
